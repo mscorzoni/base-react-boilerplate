24c659bcc885d73bb1ee347b4106ddcb683a0071
Marcos Scorzoni <mlessa@gmail.com>
2018-10-07 11:11:40 +0100

    add pagination for search

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Search from './components/search.js'
 import Table from './components/table.js'
+import Button from './components/button'
 
 
 const DEFAULT_QUERY = 'rails';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-
-
-
-
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage='
 
 class App extends Component {
 
@@ -44,7 +44,17 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+    const oldHits = page !== 0
+      ? this.state.result.hits
+      : [];
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ]
+    this.setState({
+      result: { hits: updatedHits, page }
+    });
   }
 
   onSearchSubmit(event) {
@@ -53,8 +63,8 @@ class App extends Component {
     event.preventDefault();
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
@@ -67,6 +77,7 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
+    const page =  (result && result.page ) || 0;
     return (
       <div className="page">
         <div className="interactions">
@@ -84,6 +95,13 @@ class App extends Component {
           onDismiss={this.onDismiss}
         />
         }
+        <div className="interactions">
+          <Button onClick={() =>
+              this.fetchSearchTopStories(searchTerm, page + 1)
+            }>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
